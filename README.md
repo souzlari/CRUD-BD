2fd8a3156e63953ba53f98d77d7f111a65af9b74
Lari Souza <larissaccsouza@gmail.com>
2021-08-13 18:09:30 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CRUD-BD
+
+A SER CONCLUÍDO
---
+++ src/routes/titulos.routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/tituloController')
+
+//listar todos os titulos da Pixar
+router.get('/pixar', controller.getAllPixar)
+
+//listar todos os titulos/get/find
+router.get('/', controller.getAll)
+
+//criar um novo titulo/post/save
+router.post('/', controller.createTitle)
+//listar um titulo/get/findById
+//atualizar uma informacao especifica num titulo/patch/findById/save
+//deletar um titulo/delete/findById/remove
+module.exports = router
